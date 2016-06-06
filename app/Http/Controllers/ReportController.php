7c2cd856b3b88c35c800553a36e1e5ade9b7af2b
joy <joyghosh826@gmail.com>
2016-06-06 19:48:31 +0600

    loaded pdf frm view

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -18,8 +18,8 @@ class ReportController extends Controller
 {
     public function getMpdf()
     {
-    	$num_of_movies = DB::table('movie')->count();
-    	$highest_rated_movie = DB::table('movie')->orderBy('RATING', 'desc')->first()->MOVIE_NAME;
+    	$data['num_of_movies'] = DB::table('movie')->count();
+    	$data['highest_rated_movie'] = DB::table('movie')->orderBy('RATING', 'desc')->first()->MOVIE_NAME;
 
 
     	$movie_id_ara = DB::table('movie')->lists('MOVIE_ID');
@@ -35,7 +35,7 @@ class ReportController extends Controller
     		}
     	}
     	$movie_name_max = DB::table('movie')->where('MOVIE_ID', $movie_id_max)->first()->MOVIE_NAME;
-    	$highest_searched_movie = $movie_name_max;
+    	$data['highest_searched_movie'] = $movie_name_max;
 
     	$movie_comment_count_max = -1;
     	$movie_id_comment_max = $movie_id_ara[0];
@@ -49,69 +49,20 @@ class ReportController extends Controller
     		}
     	}
 
-    	$movie_name_comment_max = DB::table('movie')->where('MOVIE_ID', $movie_id_comment_max)->first()->MOVIE_NAME;
+    	$data['movie_name_comment_max'] = DB::table('movie')->where('MOVIE_ID', $movie_id_comment_max)->first()->MOVIE_NAME;
 
-    	$num_of_users = DB::table('users')->count();
+    	$data['num_of_users'] = DB::table('users')->count();
 
-
-
-    	ob_start();
-    	$html = ob_get_clean();
-    	$html = utf8_encode($html);
-
-    	$html .= 
-    	'<html>	
-    		<head><style type="text/css">
-			.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;border:none;margin:0px auto;}
-			.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
-			.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}
-			.tg .tg-yw4l{vertical-align:top}
-			.tg .tg-6k2t{background-color:#D2E4FC;vertical-align:top}
-			</style></head>
-			<body>
-				<center>
-					<h1>Movie360</h1>
-				</center>
-				<h2>Report on this website</h2>
-				<p>
-					<table class="tg" width="80%">
-					  <tr>
-					    <th class="tg-yw4l"></th>
-					    <th class="tg-yw4l"></th>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Total number of movies</td>
-					    <td class="tg-6k2t">'.$num_of_movies.'</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-yw4l">Highest Rated Movie</td>
-					    <td class="tg-yw4l">'.$highest_rated_movie.'</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Highest Searched Movie</td>
-					    <td class="tg-6k2t">'.$highest_searched_movie.'</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-yw4l">Most Commented Movie</td>
-					    <td class="tg-yw4l">'.$movie_name_comment_max.'</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Number of users</td>
-					    <td class="tg-6k2t">'.$num_of_users.'</td>
-					  </tr>
-					</table>
-				</p>
-			</body>
-		</html>';
+	    $html = view('pdfview')->with('data', $data)->render();
 
 
-	$mpdf = new mPDF();
-	$mpdf->showImageErrors = true;
-	$mpdf->allow_charset_conversion = true;
-	$mpdf->charset_in = 'UTF-8';
-	$mpdf->WriteHTML($html);
-	$mpdf->Output('movie360', 'I');
-	exit();
-
+		$mpdf = new mPDF();
+		$mpdf->showImageErrors = true;
+		$mpdf->allow_charset_conversion = true;
+		$mpdf->charset_in = 'UTF-8';
+		$mpdf->setFooter('{PAGENO} / {nb}');
+		$mpdf->WriteHTML($html);
+		$mpdf->Output('movie360', 'I');
+		exit();
     }
 }
--- resources/views/pdfview.blade.php
+++ resources/views/pdfview.blade.php
@@ -1,43 +1,43 @@
-		<html>	
-    		<head><style type="text/css">
-			.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;border:none;margin:0px auto;}
-			.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
-			.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}
-			.tg .tg-yw4l{vertical-align:top}
-			.tg .tg-6k2t{background-color:#D2E4FC;vertical-align:top}
-			</style></head>
-			<body>
-				<center>
-					<h1>Movie360</h1>
-				</center>
-				<h2>Report on this website</h2>
-				<p>
-					<table class="tg" width="80%">
-					  <tr>
-					    <th class="tg-yw4l"></th>
-					    <th class="tg-yw4l"></th>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Total number of movies</td>
-					    <td class="tg-6k2t">{{$data['num_of_movies']}}</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-yw4l">Highest Rated Movie</td>
-					    <td class="tg-yw4l">{{$data['highest_rated_movie']}}</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Highest Searched Movie</td>
-					    <td class="tg-6k2t">{{$data['highest_searched_movie']}}</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-yw4l">Most Commented Movie</td>
-					    <td class="tg-yw4l">{{$data['movie_name_comment_max']}}</td>
-					  </tr>
-					  <tr>
-					    <td class="tg-6k2t">Number of users</td>
-					    <td class="tg-6k2t">{{$data['$num_of_users']}}</td>
-					  </tr>
-					</table>
-				</p>
-			</body>
-		</html>
+<html>	
+	<head><style type="text/css">
+	.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;border:none;margin:0px auto;}
+	.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+	.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}
+	.tg .tg-yw4l{vertical-align:top}
+	.tg .tg-6k2t{background-color:#D2E4FC;vertical-align:top}
+	</style></head>
+	<body>
+		<center>
+			<h1>Movie360</h1>
+		</center>
+		<h2>Report on this website</h2>
+		<p>
+			<table class="tg" width="80%">
+			  <tr>
+			    <th class="tg-yw4l"></th>
+			    <th class="tg-yw4l"></th>
+			  </tr>
+			  <tr>
+			    <td class="tg-6k2t">Total number of movies</td>
+			    <td class="tg-6k2t">{{$data['num_of_movies']}}</td>
+			  </tr>
+			  <tr>
+			    <td class="tg-yw4l">Highest Rated Movie</td>
+			    <td class="tg-yw4l">{{$data['highest_rated_movie']}}</td>
+			  </tr>
+			  <tr>
+			    <td class="tg-6k2t">Highest Searched Movie</td>
+			    <td class="tg-6k2t">{{$data['highest_searched_movie']}}</td>
+			  </tr>
+			  <tr>
+			    <td class="tg-yw4l">Most Commented Movie</td>
+			    <td class="tg-yw4l">{{$data['movie_name_comment_max']}}</td>
+			  </tr>
+			  <tr>
+			    <td class="tg-6k2t">Number of users</td>
+			    <td class="tg-6k2t">{{$data['num_of_users']}}</td>
+			  </tr>
+			</table>
+		</p>
+	</body>
+</html>
